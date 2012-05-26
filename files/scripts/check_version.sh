9ebd3cc22d1e0e91d5432d09e3d2040018a2e3fe
Gediminas <fzr600@gmail.com>
2012-05-26 19:59:03 +0300

    restart mule in any way

--- files/scripts/check_version.sh
+++ files/scripts/check_version.sh
@@ -6,16 +6,20 @@
 
 check_version_with_retry(){
         DIR=`dirname $0`
+		exit_code=2
         for i in {1..20}
         do
                 java -jar $DIR/check_jmx.jar -U service:jmx:rmi:///jndi/rmi://localhost:$1/jmxrmi -O $2 -A Version -c "$version"
                 if [ $? == 0 ] ; then
-                        exit 0
+                        exit_code=0
+						break;
                 fi
                 sleep 2
         done
         echo "CRITICAL: Expected version not found."
-        exit 2
+		echo "Restarting Mule..."
+		/etc/init.d/mule restart
+        exit $exit_code
 }
 
 echo "Expecting version: $version"
